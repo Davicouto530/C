faa3a5482a24e1c64896a37313aecfb030a186ee
Davi do couto <coutodavi530@gmail.com>
2024-10-23 13:58:18 -0300

    Começando a linguagem C

---
+++ src/boasvindas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    //  Adicionar um pequeno cabeçalho
+
+    printf("----------Pequeno cabeçalho----------");
+    printf("\nSeja bem vindo ao nosso programa\n");
+    printf("-----------------fim-----------------\n");
+
+    return 0;
+}
---
+++ src/hello.c
@@ -0,0 +1,12 @@
+// Importação da biblioteca padrão para o uso de teclado e monitor
+#include <stdio.h>
+
+int main(){
+// Para exibir uma mensagem na tela iremos usar o comando printf(aqui vai a mensagem formatando)
+
+printf("Hello, world!\n");
+
+// Informar a o computador que o prgrama acabou 
+
+return 0;
+}
