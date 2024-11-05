a8addf0167ff5d2ac9484295b2a1a146c2ce55e2
Davi do couto <coutodavi530@gmail.com>
2024-11-05 17:02:05 -0300

    Novo exercicios de char

---
+++ src/texto1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Declarar uma variável para armazenar o primeiro nome de um aluno
+    char nome[5] = {"Bruno"};
+
+    printf("%s\n", nome);
+
+    return 0;
+}
