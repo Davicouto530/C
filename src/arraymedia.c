ab3d24c9cdb7d81c9f8b48bc712cbf5ca91b3373
Davi do couto <coutodavi530@gmail.com>
2024-11-05 16:49:12 -0300

    Novos exercícios de array

--- src/arraymedia.c
+++ src/arraymedia.c
@@ -4,12 +4,14 @@ int main() {
     int n[] = {13,8,23,95,47,14,10,45,165};
     int p;
     int soma = 0;
-    
+    int media;
+
     for(int p = 0; p <= 8; p++) {
         soma += n[p];
+        media = soma / 9;
     }
 
-    printf("O resultado da soma dos números deu %d\n", soma);
+    printf("O valor da soma é %d, e o valor da média é %d\n", soma, media);
 
     return 0;
 }
